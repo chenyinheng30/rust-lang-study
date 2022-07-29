ef9c9f0e4786c7bacb4e62f50b4e4f75ad24c64f
chenyinheng30 <chanyinheng30@outlook.com>
2022-07-30 00:27:23 +0800

    rust 变量的隐藏(Shadowing)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
     let x = 5;
+
+    let x = x + 1;//并非为x赋值，这里重新定义了一个x
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    /////////////////////////////////////////////
+    // The value of x in the inner scope is: 12//
+    // The value of x is: 6                    //
+    /////////////////////////////////////////////
 }
